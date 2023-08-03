e5ba89883e436c94f6007f26107e98eca528dec2
jkmunoz <juliannakmunoz@gmail.com>
2023-08-03 15:40:32 -0500

    Initial Commit

---
+++ truthy-falsey/main.js
@@ -0,0 +1,22 @@
+// We can use True OR we can you 'yes' and get the same result!
+var iLikeChocolate = true;
+// var iLikeChocolate = 'Yes';
+
+if (iLikeChocolate) {
+    console.log('Eat the chocolate!');
+} else {
+    console.log('Do NOT eat the gross chocolate.')
+}
+
+
+// Examples of TYPES and if they are always 'truthy' or 'falsey'.
+// Uncomment and test in dev tools to get the answers. 
+
+// if ('hi') { console.log("truthy") } else { console.log("falsey") };
+// if (null) { console.log("truthy") } else { console.log("falsey") };
+// if (0) { console.log("truthy") } else { console.log("falsey") };
+// if ('') { console.log("truthy") } else { console.log("falsey") };
+// if (214) { console.log("truthy") } else { console.log("falsey") };
+// if (-3.14) { console.log("truthy") } else { console.log("falsey") };
+// if (undefined) { console.log("truthy") } else { console.log("falsey") };
+// if (NaN) { console.log("truthy") } else { console.log("falsey") };
