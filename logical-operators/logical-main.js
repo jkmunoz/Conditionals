5d63cc63d05999eb239ec2ecbcecf66732f9a408
jkmunoz <juliannakmunoz@gmail.com>
2023-08-03 16:25:35 -0500

    logical-main.js and index.html error free

--- logical-operators/logical-main.js
+++ logical-operators/logical-main.js
@@ -4,13 +4,24 @@
 // ! (not / the opposite)
 
 var hungry = true;
-var timeOfDay = 'Lunch time' || 'Dinner time';
+var timeOfDay = 'Dinner time';
 
-if (hungry === true && timeOfDay === 'Lunch time' || 'Dinner time') {
-    console.log('Time to eat!');
-} else if (hungry === true && timeOfDay !== 'Lunch time' || 'Dinner time') {
+if (hungry === true && (timeOfDay === 'Lunch time')) {
+    console.log('Eat lunch!');
+} else if (hungry === true && (timeOfDay !== 'Lunch time' || 'Dinner time')) {
     console.log('Eat a snack!');
 } else {
-    console.log('It is NOT time to eat.')
+    console.log('It is NOT time to eat.');
 }
 
+// OR try...
+
+// if (hungry === true && timeOfDay === 'Lunch time') {
+//     console.log('Eat lunch!');
+// } else if (hungry === true && timeOfDay === 'Dinner time') {
+//     console.log('Eat dinner!');
+// } else if (hungry === true && timeOfDay !== 'Lunch time') {
+//     console.log('Eat a snack!');
+// } else {
+//     console.log('It is NOT time to eat.')
+// }
